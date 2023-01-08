2b4c4661a4ab4ce1584eea6442c3fd9aecc3dcf5
PauloPortofolio <pvba02@gmail.com>
2023-01-08 15:19:00 -0500

    8.0) Java 17 Certif-Exam-Prep  - Folder refactoring  - Test Casting strategy  - Variable names special chracteres test

---
+++ src/main/java/VariablesDeclarationTest.java
@@ -0,0 +1,13 @@
+public class VariablesDeclarationTest {
+
+  public static void main(String[] args) {
+
+    // String _ = "Accepted_A"; //'_' is a keyword
+    String _A = "Accepted_A";
+    String $ = "Accepted$";
+    String $_ = "Accepted_$";
+
+    System.out.println("Accepted characteres:" + $ + _A + $_);
+
+  }
+}
